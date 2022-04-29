34a99c9167d489dd9bbcf7020e4d31b831764460
imdadadelabou0 <imdadadelabou0@gmail.com>
2022-04-29 19:56:18 +0100

    test serve

--- myApp.js
+++ myApp.js
@@ -4,12 +4,12 @@ var app = express();
 
 
 console.log("Hello World");
-app.use(express.static(__dirname + '/public'));
+app.use("/public", express.static(__dirname + '/public'));
 app.get('/', (req, res) => {
     // res.send("Hello Express");
     return res.sendFile(__dirname + "/views/index.html");
 });
-app.listen(3000);
+// app.listen(3000);
 
 
 

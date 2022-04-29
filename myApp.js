7895e8242f957f46a549be8d98053cd1bf9f1ae5
imdadadelabou0 <imdadadelabou0@gmail.com>
2022-04-29 17:44:39 +0100

    Create middleware to send file to a client

--- myApp.js
+++ myApp.js
@@ -5,10 +5,11 @@ var app = express();
 
 console.log("Hello World");
 app.get('/', (req, res) => {
-    res.send("Hello Express");
+    // res.send("Hello Express");
+    return res.sendFile(__dirname + "/views/index.html");
 });
 
-
+// app.listen(3000);
 
 
 

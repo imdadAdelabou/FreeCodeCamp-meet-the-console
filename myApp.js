98c3217e42eaa6e0f9db9cc716705f8f5ae2e4f4
imdadadelabou0 <imdadadelabou0@gmail.com>
2022-04-29 17:16:04 +0100

    complete the first task

--- myApp.js
+++ myApp.js
@@ -1,7 +1,14 @@
 var express = require('express');
+const res = require('express/lib/response');
 var app = express();
 
+
 console.log("Hello World");
+app.get('/', () => {
+    return res.sand("Hello Express");
+});
+
+
 
 
 
